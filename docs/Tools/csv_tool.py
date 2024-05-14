488a998eb5f1e7cf6fe9688a903d88f0c5bfe187
Austin Jung <122995391+JungHeeyun@users.noreply.github.com>
2024-05-14 16:13:55 +0800

    Update csv_tool.py

--- docs/Tools/csv_tool.py
+++ docs/Tools/csv_tool.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 
 def csv(csv_path, query):
     agent = create_csv_agent(
-        ChatOpenAI(temperature=0, model="gpt-4-turbo-2024-04-09"),#use gpt 3.5 turbo for the faster responese from this tool.
+        ChatOpenAI(temperature=0, model="gpt-4o-2024-05-13"),# updated to gpt-4o-2024-05-13 model 
         csv_path,
         verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
